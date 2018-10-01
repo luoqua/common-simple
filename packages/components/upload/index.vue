3dc1d1b5a7dbfc66189bff1323fafccad90e7316
luoqua <1132228219@qq.com>
2018-10-01 23:32:30 +0800

    update upload

---
+++ packages/components/upload/index.vue
@@ -0,0 +1,26 @@
+<template>
+	
+</template>
+
+
+
+<script type="text/javascript">
+import create from '@/utils/create'
+
+export default create({
+	name: 'upload',
+	props: [],
+	data() {
+		return {
+		}
+	},
+	methods: {
+
+	}
+})
+</script>
+
+
+<style type="text/css" lang="scss" scoped>
+
+</style>
