4b3df33a5033d30969319640f3635df68093cddf
luoqua <1132228219@qq.com>
2018-10-11 00:24:19 +0800

    update

--- packages/components/progress/index.vue
+++ packages/components/progress/index.vue
@@ -1,12 +1,12 @@
 <template>
-	<ul :class="b()" class="mod-uploader">
-		<li :class="b('item')" class="mod-uploader__item" id="uploadItemWU_FILE_4">
-	        <div class="mod-uploader__meta">
-	            <span class="mod-uploader__name">snsvideodownload.png</span>
-	            <em class="mod-uploader__size">(155.62K)</em>
+	<ul :class="b()">
+		<li :class="b('item')" >
+	        <div :class="b('meta')" >
+	            <span :class="b('name')" >snsvideodownload.png</span>
+	            <em :class="b('size')" >(155.62K)</em>
 	        </div>
-	        <div class="mod-uploader__process">
-	            <div class="mod-uploader__process-done" style="width: 20.3657%; transition: width 0.3s ease 0s;"></div>
+	        <div :class="b('process')" >
+	            <div :class="b('process-done')"  style="width: 20.3657%; transition: width 0.3s ease 0s;"></div>
 	        </div>
 	    </li>
 	</ul>
@@ -42,57 +42,46 @@ export default create({
 	    background-clip: padding-box;
 	    position: relative;
 	    background-color: #fff;
-	    position: relative; 
+	    position: relative;
 	    display: block;
+	    font-size:14px;
 	    $p:&;
+	    &__item{
+	    	border-bottom: 1px solid #e7e7eb;
+		    padding: 5px 10px;
+		    position: relative;
+	    }
+	    &__name{
+	    	display: inline-block;
+		    *display: inline;
+		    *zoom:1;width: 100px;
+		    overflow: hidden;
+		    text-overflow: ellipsis;
+		    white-space: nowrap;
+		    max-width: 100%;
+		    vertical-align: middle
+	    }
+	    &__size{
+	    	color: #7b7b7b;
+	    	font-style: normal
+	    }
+	    &__process{
+	    	height: 5px;
+		    width: 100px;
+		    background: #e1e3e6;
+		    border-radius: 4px;
+		    background-clip: padding-box;
+		    position: absolute;
+		    top: 14px;
+		    right: 10px
+	    }
+	    &__process-done{
+	    	background: #5aa1dd;
+		    position: absolute;
+		    height: 5px;
+		    border-radius: 2px;
+		    background-clip: padding-box
+	    }
 	}
 
-	.mod-uploader__item {
-	    border-bottom: 1px solid #e7e7eb;
-	    padding: 5px 10px;
-	    position: relative
-	}
-
-	.mod-uploader__item:last-child {
-	    border: none
-	}
-
-	.mod-uploader__name {
-	    display: inline-block;
-	    *display: inline;
-	    *zoom:1;width: 100px;
-	    overflow: hidden;
-	    text-overflow: ellipsis;
-	    white-space: nowrap;
-	    max-width: 100%;
-	    vertical-align: middle
-	}
-
-	.mod-uploader__size {
-	    color: #7b7b7b;
-	    font-style: normal
-	}
-
-	.mod-uploader__process {
-	    height: 5px;
-	    width: 100px;
-	    background: #e1e3e6;
-	    border-radius: 4px;
-	    background-clip: padding-box;
-	    position: absolute;
-	    top: 14px;
-	    right: 10px
-	}
-
-	.mod-uploader__process-done {
-	    background: #5aa1dd;
-	    position: absolute;
-	    height: 5px;
-	    border-radius: 2px;
-	    background-clip: padding-box
-	}
-
-	.mod-uploader__process-red {
-	    background-color: #e15f63
-	}
 </style>
