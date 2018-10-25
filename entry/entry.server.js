d96f70b7480d1bdd49cbf1cea1520dc0668c5f87
luoqua <1132228219@qq.com>
2018-10-25 11:46:15 +0800

    test jenkins

--- entry/entry.server.js
+++ entry/entry.server.js
@@ -11,7 +11,9 @@ export default context => {
 		const matchedComponents = app.$router.getMatchedComponents()
 
 		// 如果没有组件，说明该路由不存在，报错404
-		if(!matchedComponents.length) { return reject({code:404}) }
+		if (!matchedComponents.length) {
+			return reject({code: 404})
+		}
 
 		// 遍历路由下所有的组件，如果有需要服务端渲染的请求，则进行请求
 		Promise.all(matchedComponents.map(component => {
